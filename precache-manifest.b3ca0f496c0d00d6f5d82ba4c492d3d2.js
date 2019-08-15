0ee82ee61d1006ce510189d7279a689193278b81
ronnelskie001 <ronnelskie001@gmail.com>
2019-08-16 07:29:19 +0800

    Updates

---
+++ precache-manifest.b3ca0f496c0d00d6f5d82ba4c492d3d2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "e20a934ba9390d7ecc4b8d08f94510d3",
+    "url": "/trainze/static/media/travelgid.e20a934b.jpg"
+  },
+  {
+    "revision": "acdcbbe910c4a360f2931a4cd4fa1012",
+    "url": "/trainze/static/media/pnr-status.acdcbbe9.jpg"
+  },
+  {
+    "revision": "9cf270da55f309856eedbd5aacde9a61",
+    "url": "/trainze/static/media/logo.9cf270da.jpg"
+  },
+  {
+    "revision": "7774317d29898246548fdb6c767ddac7",
+    "url": "/trainze/static/media/Final_Map2.7774317d.jpg"
+  },
+  {
+    "revision": "8458db8318e2f88404c8",
+    "url": "/trainze/static/js/runtime~main.4b778817.js"
+  },
+  {
+    "revision": "21e0839a0340a0192894",
+    "url": "/trainze/static/js/main.8dbe30f2.chunk.js"
+  },
+  {
+    "revision": "e3974443c12721decbd2",
+    "url": "/trainze/static/js/2.63a6d470.chunk.js"
+  },
+  {
+    "revision": "21e0839a0340a0192894",
+    "url": "/trainze/static/css/main.adbd8cd6.chunk.css"
+  },
+  {
+    "revision": "e3974443c12721decbd2",
+    "url": "/trainze/static/css/2.9a60604e.chunk.css"
+  },
+  {
+    "revision": "6115e11c8e74d29615a708862b54d020",
+    "url": "/trainze/index.html"
+  }
+];
